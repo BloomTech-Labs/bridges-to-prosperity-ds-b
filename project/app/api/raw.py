50ff08a491a7c38f9f9de6d01b7b5a8d652c57b1
Lord-Kanzler <climberalex@gmail.com>
2020-08-24 16:05:13 -0400

    ran back to fix formatting and ensuring readability

--- project/app/api/raw.py
+++ project/app/api/raw.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 import pandas as pd
 import json
+
 router = APIRouter()
 
-site_assessment = 'https://raw.githubusercontent.com/Lambda-School-Labs/Labs25-Bridges_to_Prosperity-TeamB-ds/main/data/raw/B2P_Rwanda_Site_Assessment_Data.csv'
+site_assessment = "https://raw.githubusercontent.com/Lambda-School-Labs/Labs25-Bridges_to_Prosperity-TeamB-ds/main/data/raw/B2P_Rwanda_Site_Assessment_Data.csv"
 site_assessment = pd.read_csv(site_assessment)
 
 
@@ -12,4 +13,3 @@ async def raw():
     output = site_assessment.to_json(orient="index")
     parsed = json.loads(output)
     return parsed
-
--- project/app/main.py
+++ project/app/main.py
@@ -5,10 +5,10 @@ import uvicorn
 from app.api import predict, viz, raw
 
 app = FastAPI(
-    title='Labs25-Bridges_to_Prosperity-TeamB-ds',
-    description='Replace this placeholder text',
-    version='0.1',
-    docs_url='/',
+    title="Labs25-Bridges_to_Prosperity-TeamB-ds",
+    description="Replace this placeholder text",
+    version="0.1",
+    docs_url="/",
 )
 
 app.include_router(predict.router)
@@ -17,11 +17,11 @@ app.include_router(raw.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app)
