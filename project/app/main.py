562318911cef27b01c7220a325f4cd5e8f45e9c2
Lord-Kanzler <climberalex@gmail.com>
2020-08-25 13:00:07 -0400

    added /villages  endpoint to return clean gov. village data and IDs

--- project/app/main.py
+++ project/app/main.py
@@ -2,25 +2,28 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from app.api import predict, viz
+from app.api import predict, viz, raw, sites, villages
 
 app = FastAPI(
-    title='YOUR-PRODUCT-TEAM-NAME DS API',
-    description='Replace this placeholder text',
-    version='0.1',
-    docs_url='/',
+    title="Labs25-Bridges_to_Prosperity-TeamB-ds",
+    description="Replace this placeholder text",
+    version="0.1",
+    docs_url="/",
 )
 
 app.include_router(predict.router)
 app.include_router(viz.router)
+app.include_router(raw.router)
+app.include_router(sites.router)
+app.include_router(villages.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app)
