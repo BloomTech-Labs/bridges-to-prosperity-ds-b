dce5113d1b78e2d2ee6da39833eac87577ee90aa
Lord-Kanzler <climberalex@gmail.com>
2020-09-16 14:24:25 -0400

    edits to output format as discussed with team

--- project/app/api/final_data.py
+++ project/app/api/final_data.py
@@ -7,51 +7,83 @@ router = APIRouter()
 sites_ids = "https://raw.githubusercontent.com/Lambda-School-Labs/Labs25-Bridges_to_Prosperity-TeamB-ds/feature/final-data-output/data/edit/B2P_Rwanda_Sites%2BIDs_full_2020-09-16.csv"
 sites_ids = pd.read_csv(sites_ids)
 
-# reordering 'sites_ids' to desired output format
+
+# Renaming Column
+sites_ids = sites_ids.rename(
+    columns={
+        "Project Code": "project_code",
+        "Bridge Site Name": "name",
+        "Village_ID": "village_id",
+        "Village": "village",
+        "Cell_ID": "cell_id",
+        "Cell": "cell",
+        "Sector_ID": "sector_id",
+        "Sector": "sector",
+        "Dist_ID": "district_id",
+        "District": "district",
+        "Province": "province",
+        "Bridge Type": "type",
+        "Project Stage": "stage",
+        "Project Sub-Stage": "sub_stage",
+        "Individuals Directly Served": "Individuals_directly_served",
+        "Span (m)": "span",
+        "GPS (Latitude)": "lat",
+        "GPS (Longitude)": "long",
+        "Communities_Served": "communities_served",
+        "Form: Form Name": "form",
+        "CaseSafeID Form": "case_safe_id",
+        "Bridge Opportunity: Opportunity ID": "opportunity_id",
+        "Country": "country",
+    }
+)
+
+
+# Reordering 'sites_ids' to desired output format
 """
 # Desired Output Format
 const bridgeSite = {
-  id: 1014107,
+  project_code: 1014107,
+  district: "whatever",
+  province: "province",
+  sector: "sector",
+  cell: "cell",
+  village: "village",
+  village_id: 342343,
   name: "Buzi",
   type: "Suspended",
   stage: "Rejected",
-  subStage: "Technical",
-  individualsDirectlyServed: 0.0,
+  sub_stage: "Technical",
+  individuals_directly_served: 0.0,
   span: "",
-  latitude: -2.42056,
-  longitude: 28.9662,
-  communitiesServed: [
-    {
-      id: 22050101,
-      name: "Agahehe",
-    },
-    {
-      id: 22050102,
-      name: "Kabacuzi",
-    },
-    {
-      id: 22050103,
-      name: "Kamutozo",
-    },
-    {
-      id: 22050104,
-      name: "Kamweko",
-    },
+  lat: -2.42056,
+  long: 28.9662,
+  communities_served: [
+      "Agahehe",
+      "Kabacuzi",
+      "Kamutozo",
+      "Kamweko",
   ],
 };
 """
 
+# Column order
 column_order = [
-    "Project Code",
-    "Bridge Site Name",
-    "Bridge Type",
-    "Project Stage",
-    "Project Sub-Stage",
-    "Individuals Directly Served",
-    "Span (m)",
-    "GPS (Latitude)",
-    "GPS (Longitude)",
-    "Communities_Served",
+    "project_code",
+    "province",
+    "district",
+    "sector",
+    "cell",
+    "village",
+    "village_id",
+    "name",
+    "type",
+    "stage",
+    "sub_stage",
+    "Individuals_directly_served",
+    "span",
+    "lat",
+    "long",
+    "communities_served",
 ]
 
 sites_ids = sites_ids.reindex(columns=column_order)
--- project/app/api/final_data_extended.py
+++ project/app/api/final_data_extended.py
@@ -7,70 +7,97 @@ router = APIRouter()
 sites_ids = "https://raw.githubusercontent.com/Lambda-School-Labs/Labs25-Bridges_to_Prosperity-TeamB-ds/feature/final-data-output/data/edit/B2P_Rwanda_Sites%2BIDs_full_2020-09-16.csv"
 sites_ids = pd.read_csv(sites_ids)
 
-# reordering 'sites_ids' to desired output format
+
+# Renaming Column
+sites_ids = sites_ids.rename(
+    columns={
+        "Project Code": "project_code",
+        "Bridge Site Name": "name",
+        "Village_ID": "village_id",
+        "Village": "village",
+        "Cell_ID": "cell_id",
+        "Cell": "cell",
+        "Sector_ID": "sector_id",
+        "Sector": "sector",
+        "Dist_ID": "district_id",
+        "District": "district",
+        "Province": "province",
+        "Bridge Type": "type",
+        "Project Stage": "stage",
+        "Project Sub-Stage": "sub_stage",
+        "Individuals Directly Served": "Individuals_directly_served",
+        "Span (m)": "span",
+        "GPS (Latitude)": "lat",
+        "GPS (Longitude)": "long",
+        "Communities_Served": "communities_served",
+        "Form: Form Name": "form",
+        "CaseSafeID Form": "case_safe_id",
+        "Bridge Opportunity: Opportunity ID": "opportunity_id",
+        "Country": "country",
+    }
+)
+
+
+# Reordering 'sites_ids' to desired output format + extension
 """
 # Desired Output Format
 const bridgeSite = {
-  id: 1014107,
+  project_code: 1014107,
+  district: "whatever",
+  province: "province",
+  sector: "sector",
+  cell: "cell",
+  village: "village",
+  village_id: 342343,
   name: "Buzi",
   type: "Suspended",
   stage: "Rejected",
-  subStage: "Technical",
-  individualsDirectlyServed: 0.0,
+  sub_stage: "Technical",
+  individuals_directly_served: 0.0,
   span: "",
-  latitude: -2.42056,
-  longitude: 28.9662,
-  communitiesServed: [
-    {
-      id: 22050101,
-      name: "Agahehe",
-    },
-    {
-      id: 22050102,
-      name: "Kabacuzi",
-    },
-    {
-      id: 22050103,
-      name: "Kamutozo",
-    },
-    {
-      id: 22050104,
-      name: "Kamweko",
-    },
+  lat: -2.42056,
+  long: 28.9662,
+  communities_served: [
+      "Agahehe",
+      "Kabacuzi",
+      "Kamutozo",
+      "Kamweko",
   ],
 };
 """
 
+# Column order
 column_order = [
-    "Project Code",
-    "Bridge Site Name",
-    "Village_ID",
-    "Village",
-    "Cell_ID",
-    "Cell",
-    "Sector_ID",
-    "Sector",
-    "Dist_ID",
-    "District",
-    "Province",
-    "Bridge Type",
-    "Project Stage",
-    "Project Sub-Stage",
-    "Individuals Directly Served",
-    "Span (m)",
-    "GPS (Latitude)",
-    "GPS (Longitude)",
-    "Communities_Served",
-    "Form: Form Name",
-    "CaseSafeID Form",
-    "Bridge Opportunity: Opportunity ID",
-    "Country"
+    "project_code",
+    "province",
+    "district",
+    "district_id",
+    "sector",
+    "sector_id",
+    "cell",
+    "cell_id",
+    "village",
+    "village_id",
+    "name",
+    "type",
+    "stage",
+    "sub_stage",
+    "Individuals_directly_served",
+    "span",
+    "lat",
+    "long",
+    "communities_served",
+    "form",
+    "case_safe_id",
+    "opportunity_id",
+    "country",
 ]
+
 
 sites_ids = sites_ids.reindex(columns=column_order)
 
-# /final-data endpoint
-@router.get("/final-data-extended")
+# /final-data/extended endpoint
+@router.get("/final-data/extended")
 async def final_data_extended():
     output = sites_ids.to_json(orient="records")
     parsed = json.loads(output)
