1e9ca6eb25bb9ea01dfde57de745455786b2246e
Lord-Kanzler <climberalex@gmail.com>
2020-09-21 19:39:27 -0400

    ran black to fix formatting

--- project/app/api/final_data.py
+++ project/app/api/final_data.py
@@ -14,7 +14,7 @@ async def final_data():
     # output = sites_ids.to_json(orient="records")
     # parsed = json.loads(output)
     # return parsed
-    '''
+    """
     Desired Format
 {
   project_code: 1014107,
@@ -39,49 +39,49 @@ async def final_data():
       "Kamweko",
   ],
 };
-    '''
+    """
 
     # Loading data from URL
     request = requests.get(
-        'https://raw.githubusercontent.com/Lambda-School-Labs/Labs25-Bridges_to_Prosperity-TeamB-ds/feature/edit-final-output/data/edit/B2P_Rwanda_Sites%2BIDs_full_2020-09-21.csv')
+        "https://raw.githubusercontent.com/Lambda-School-Labs/Labs25-Bridges_to_Prosperity-TeamB-ds/feature/edit-final-output/data/edit/B2P_Rwanda_Sites%2BIDs_full_2020-09-21.csv"
+    )
     buff = io.StringIO(request.text)
     directread = csv.DictReader(buff)
-    
+
     output = {}
-    
-    
+
     # Loop over rows and return according to desired format
     for row in directread:
-        
-        # splitting "communities_served" into list of strings with every iteration
-        if len(row['communities_served']) == 0:
+
+        # splitting "communities_served" into list of strings with every
+        # iteration
+        if len(row["communities_served"]) == 0:
             communities_served = "NO COMMUNITIES SERVED"
         else:
-            communities_served = list(row['communities_served'].split(", "))
-        
-        
+            communities_served = list(row["communities_served"].split(", "))
+
         # Set key for dictionary
-        key = row['project_code']
-        
+        key = row["project_code"]
+
         # Set output format
         output[key] = {
-            "project_code": row['project_code'],
-            "province": row['province'],
-            "district": row['district'],
-            "sector": row['sector'],
-            "cell": row['cell'],
-            "village": row['village'],
-            "village_id": row['village_id'],
-            "name": row['name'],
-            "type": row['type'],
-            "stage": row['stage'],
-            "sub_stage": row['sub_stage'],
-            "Individuals_directly_served": int(row['Individuals_directly_served']),
-            "span": int(row['span']),
-            "lat": float(row['lat']),
-            "long": float(row['long']),
-            "communities_served": communities_served
+            "project_code": row["project_code"],
+            "province": row["province"],
+            "district": row["district"],
+            "sector": row["sector"],
+            "cell": row["cell"],
+            "village": row["village"],
+            "village_id": row["village_id"],
+            "name": row["name"],
+            "type": row["type"],
+            "stage": row["stage"],
+            "sub_stage": row["sub_stage"],
+            "Individuals_directly_served": int(row["Individuals_directly_served"]),
+            "span": int(row["span"]),
+            "lat": float(row["lat"]),
+            "long": float(row["long"]),
+            "communities_served": communities_served,
         }
-        
+
     # Return output
-    return output 
+    return output
--- project/app/api/final_data_extended.py
+++ project/app/api/final_data_extended.py
@@ -8,42 +8,14 @@ import requests
 router = APIRouter()
 
 
-# Column order
-column_order = [
-    "project_code",
-    "province",
-    "district",
-    "district_id",
-    "sector",
-    "sector_id",
-    "cell",
-    "cell_id",
-    "village",
-    "village_id",
-    "name",
-    "type",
-    "stage",
-    "sub_stage",
-    "Individuals_directly_served",
-    "span",
-    "lat",
-    "long",
-    "communities_served",
-    "form",
-    "case_safe_id",
-    "opportunity_id",
-    "country",
-]
-
-
 # /final-data/extended endpoint
 @router.get("/final-data/extended")
 async def final_data_extended():
 
-
     # Loading data from URL
     request = requests.get(
-        'https://raw.githubusercontent.com/Lambda-School-Labs/Labs25-Bridges_to_Prosperity-TeamB-ds/feature/edit-final-output/data/edit/B2P_Rwanda_Sites%2BIDs_full_2020-09-21.csv')
+        "https://raw.githubusercontent.com/Lambda-School-Labs/Labs25-Bridges_to_Prosperity-TeamB-ds/feature/edit-final-output/data/edit/B2P_Rwanda_Sites%2BIDs_full_2020-09-21.csv"
+    )
     buff = io.StringIO(request.text)
     directread = csv.DictReader(buff)
 
@@ -52,43 +24,42 @@ async def final_data_extended():
     # Loop over rows and return according to desired format
     for row in directread:
 
-        # splitting "communities_served" into list of strings with every iteration
-        if len(row['communities_served']) == 0:
+        # splitting "communities_served" into list of strings with every
+        # iteration
+        if len(row["communities_served"]) == 0:
             communities_served = "NO COMMUNITIES SERVED"
         else:
-            communities_served = list(row['communities_served'].split(", "))
-            
-            
-        # Set key for dictionary
-        key = row['project_code']
+            communities_served = list(row["communities_served"].split(", "))
 
+        # Set key for dictionary
+        key = row["project_code"]
 
         # Set output format
         output[key] = {
-            "project_code": row['project_code'],
-            "province": row['province'],
-            "district": row['district'],
-            "district_id": row['district_id'],
-            "sector": row['sector'],
-            "sector_id": row['sector_id'],
-            "cell": row['cell'],
-            "cell_id": row['cell_id'],
-            "village": row['village'],
-            "village_id": row['village_id'],
-            "name": row['name'],
-            "type": row['type'],
-            "stage": row['stage'],
-            "sub_stage": row['sub_stage'],
-            "Individuals_directly_served": int(row['Individuals_directly_served']),
-            "span": int(row['span']),
-            "lat": float(row['lat']),
-            "long": float(row['long']),
+            "project_code": row["project_code"],
+            "province": row["province"],
+            "district": row["district"],
+            "district_id": row["district_id"],
+            "sector": row["sector"],
+            "sector_id": row["sector_id"],
+            "cell": row["cell"],
+            "cell_id": row["cell_id"],
+            "village": row["village"],
+            "village_id": row["village_id"],
+            "name": row["name"],
+            "type": row["type"],
+            "stage": row["stage"],
+            "sub_stage": row["sub_stage"],
+            "Individuals_directly_served": int(row["Individuals_directly_served"]),
+            "span": int(row["span"]),
+            "lat": float(row["lat"]),
+            "long": float(row["long"]),
             "communities_served": communities_served,
-            "form": row['form'],
-            "case_safe_id": row['case_safe_id'],
-            "opportunity_id": row['opportunity_id'],
-            "country": row['country'],
-        }
+            "form": row["form"],
+            "case_safe_id": row["case_safe_id"],
+            "opportunity_id": row["opportunity_id"],
+            "country": row["country"],
+            }
 
     # Return output
     return output
